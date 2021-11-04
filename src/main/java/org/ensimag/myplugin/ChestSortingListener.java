f606f5177416464d79e76f1472f32d80415ccda5
Elias Bina <elias.bina@grenoble-inp.org>
2021-11-04 16:50:41 +0100

    Cringe hein

--- src/main/java/org/ensimag/myplugin/ChestSortingListener.java
+++ src/main/java/org/ensimag/myplugin/ChestSortingListener.java
@@ -2,6 +2,7 @@ package org.ensimag.myplugin;
 
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
+import org.bukkit.block.DoubleChest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.BlockInventoryHolder;
@@ -12,7 +13,7 @@ public class ChestSortingListener implements Listener{
     @EventHandler
     public void onInventoryClick(InventoryOpenEvent event){
         Inventory inv = event.getInventory();
-        if(inv.getHolder() instanceof BlockInventoryHolder){
+        if(inv.getHolder() instanceof BlockInventoryHolder || inv.getHolder() instanceof DoubleChest){
             Bukkit.getLogger().info("Chest Opened");
         }else{
             Bukkit.getLogger().info("Not a chest");
