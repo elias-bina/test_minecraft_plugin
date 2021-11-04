a122f793594db2477e36afbf3c7261bd5afe5c57
jaubertb <baudouin.jaubert@gmail.com>
2021-11-04 14:13:09 +0100

    initial commit 2.0

--- src/main/java/org.ensimag.myplugin/PluginMain.java
+++ src/main/java/org.ensimag.myplugin/PluginMain.java
@@ -1,7 +1,8 @@
-package test;
+package org.ensimag.myplugin;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class App extends JavaPlugin {
+public class PluginMain extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
