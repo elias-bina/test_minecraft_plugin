44331c526cc13d67c33020f463b20b3588fef7ab
Elias Bina <ebina@tetrane.com>
2021-11-06 15:01:54 +0100

    Added "Smart" sorting

--- src/main/java/org/ensimag/myplugin/chestsorting/ChestSortingListener.java
+++ src/main/java/org/ensimag/myplugin/chestsorting/ChestSortingListener.java
@@ -16,6 +16,8 @@ import org.bukkit.inventory.ItemStack;
 
 public class ChestSortingListener implements Listener{
 
+    public static int INVENTORY_LINE_SIZE = 9;
+
     private static ChestSortingListener instance;
 
     private ChestSortingListener(){
@@ -35,29 +37,30 @@ public class ChestSortingListener implements Listener{
         Inventory inv = event.getInventory();
         Boolean isOn = SortingCommand.getInstance().getChestSortingPerPlayer().get(event.getPlayer().getName());
         if (isOn != null && isOn) {
-            if (inv.getHolder() instanceof BlockInventoryHolder || inv.getHolder() instanceof DoubleChest) {
+            if ((inv.getHolder() instanceof BlockInventoryHolder && inv.getSize() > 5) || inv.getHolder() instanceof DoubleChest) {
                 Bukkit.getLogger().info("Chest sorted");
-                sortInventory(inv);
+                sortInventory(inv, false); // Sorts for compressing
                 compressInventory(inv);
+                sortInventory(inv, true); // Reorganizes after compression
             }
         }
     }
 
-    void sortInventory(Inventory inv){
+    void sortInventory(Inventory inv, boolean reorganize){
 
         List<ItemStack> items = Arrays.asList(inv.getContents());
 
+        if(reorganize){
+            List<List<ItemStack>> itemLists = groupItems(items);
+            items = smartSort(itemLists, inv.getSize());
+        } else {
+            items.sort(new SortbyName());
+        }
 
-        // Bukkit.getLogger().info(items.toString());
-
-        // TODO: More inteligent sorting
-        items.sort(new SortbyName());
         inv.clear();
 
-
-        // Bukkit.getLogger().info(items.toString());
         ItemStack[] i = items.toArray(ItemStack[]::new);
-
+    
         inv.setContents(i);
     }
 
@@ -112,6 +115,94 @@ public class ChestSortingListener implements Listener{
         inv.setContents(i);
     }
 
+    List<List<ItemStack>> groupItems(List<ItemStack> items){
+        // Group items of same type in lists
+        Map<Material, List<ItemStack>> itemMap = new HashMap<Material, List<ItemStack>>();
+        for (ItemStack itemStack : items) {
+            if(itemStack != null){
+                List<ItemStack> stackList = itemMap.get(itemStack.getType());
+                if(stackList == null){
+                    stackList = new LinkedList<ItemStack>();
+                    itemMap.put(itemStack.getType(), stackList);
+                }
+                stackList.add(itemStack);
+            }
+        }
+        // Convert Map to List
+        List<List<ItemStack>> res = new LinkedList<List<ItemStack>>();
+        itemMap.forEach((k, v) -> res.add(v)); 
+
+        // Sort by list size
+        res.sort(new SortbyLen());
+
+        return res;
+    }
+
+    List<ItemStack> smartSort(List<List<ItemStack>> itemLists, int inventorySize){
+        final int maxLine = inventorySize/INVENTORY_LINE_SIZE;
+
+        List<ItemStack> items = new LinkedList<>();
+        int lineCompletion = 0;
+        int invLine = 0;
+
+        // Premier tour de l'inv où on essaie de mettre bien
+
+        while(invLine < maxLine){
+            lineCompletion = 0;
+            for(List<ItemStack> itemList : itemLists){
+                // On ne met des grosses listes d'items  qu'en début de ligne
+                while(itemList.size() > INVENTORY_LINE_SIZE && lineCompletion == 0 && invLine < maxLine){
+                    for(int i = 0; i<INVENTORY_LINE_SIZE; i++){
+                        moveFirstItemStack(itemList, items);
+                        lineCompletion++;
+                    }
+                    lineCompletion = 0;
+                    invLine++;
+                }
+
+                int size = itemList.size();
+                // Complète la ligne avec une liste d'objets pas trop grande
+                if(size + lineCompletion <= INVENTORY_LINE_SIZE && size > 0 && invLine < maxLine){
+                    for(int i = 0; i<size; i++){
+                        moveFirstItemStack(itemList, items);
+                        lineCompletion++;
+                    }
+                }
+            }
+            // On complète les lignes 
+            while(lineCompletion < INVENTORY_LINE_SIZE){
+                items.add(null);
+                lineCompletion++;
+            }
+            lineCompletion = 0;
+            invLine++;
+        }
+
+        // Deuxième tour de l'inv où on met les invendus
+        int actualSlot = 0;
+        while(actualSlot < inventorySize){
+            for(List<ItemStack> itemList : itemLists){
+                int size = itemList.size();
+                if(size > 0 && actualSlot < inventorySize){
+                    for(int i = 0; i < size; i++){
+                        while(items.get(actualSlot) != null) actualSlot++;
+                        moveFirstItemStack(itemList, items);
+                        actualSlot++;
+                    }
+                }
+            }
+            actualSlot++;
+        }
+
+        return items;
+    }
+
+    void moveFirstItemStack(List<ItemStack> src, List<ItemStack> dest){
+        dest.add(src.get(0));
+        src.remove(0);
+    }
+
+
 }
 
 
@@ -130,5 +221,22 @@ class SortbyName implements Comparator<ItemStack>
             return -1;
         }
         return a.getType().compareTo(b.getType()) ;
+    }
+}
+
+class SortbyLen implements Comparator<List<ItemStack>>
+{
+    public int compare(List<ItemStack> a, List<ItemStack> b)
+    {
+        if(a == null){
+            if(b == null){
+                return 0;
+            }
+            return 1;
+        }
+        if(b == null){
+            return -1;
+        }
+        return b.size() - a.size();
     }
 }
